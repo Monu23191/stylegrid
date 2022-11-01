4aae7a0d6750fc50dae41c6872c968b6e844a881
ranjnadoors <ranjna@doors4s.com>
2022-11-01 13:53:06 +0530

    website style.css

--- resources/views/member/website/includes/header.blade.php
+++ resources/views/member/website/includes/header.blade.php
@@ -2,7 +2,8 @@
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
-    <link rel="stylesheet" type="text/css" href="{{ asset('member/website/assets/css/bootstrap.css') }}">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/css/bootstrap.min.css" >
+    <!-- <link rel="stylesheet" type="text/css" href="{{ asset('member/website/assets/css/bootstrap.css') }}"> -->
     <link rel="stylesheet" type="text/css" href="{{ asset('member/website/assets/css/bootstrap-extended.css') }}">
     <link rel="stylesheet" href="{{ asset('member/website/assets/css/style.css') }}">
 </head>
