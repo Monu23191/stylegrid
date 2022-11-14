7e5ee0e8e29450945b076d26bfb83b9d5d073080
monu <monu.joon@unoecom.com>
2022-11-14 16:52:51 +0530

    view grid

---
+++ resources/views/stylist/postloginview/view_grid.blade.php
@@ -0,0 +1,133 @@
+@include("stylist.postloginview.partials.header.header")
+@include("stylist.postloginview.partials.navigate.navigate")
+<meta name="csrf-token" content="{{ csrf_token() }}">
+ <!-- BEGIN: Content-->
+
+     <!-- BEGIN: Content-->
+    <div class="app-content content bg-white">
+        <div class="content-wrapper">
+
+            <div class="content-header row">
+            </div>
+            <div class="content-body">
+                <!-- Revenue, Hit Rate & Deals -->
+                <div class=" mt-lg-3 row">
+                    <div class="col-8">
+                        <h1>Browse your recent StyleGrid’s.</h1>
+                        <h3>Look through all your grids in one place.</h3>
+                        <a href=""><button class="grid-btn">Create Grid</button></a>
+                    </div>
+                    <div class="col-4 quick-link text-right">
+                        <span class="mr-lg-5"><a hrf="">Quick Link</a></span>
+                        <div class="d-flex justify-content-end my-2">
+                            <a href="" class="mx-lg-1"><img src="app-assets/images/icons/Chat.svg" alt=""></a>
+                            <a href="" class="mx-1"><img src="app-assets/images/icons/File Invoice.svg" alt=""></a>
+                            <a href="" class="mx-lg-1"><img src="app-assets/images/icons/Gear.svg" alt=""></a>
+
+                        </div>
+
+                    </div>
+                </div>
+                <!-------------------- fulfil souring request--------->
+                <div id="create-grid-1" class="mt-2">
+                    <div class="row">
+                        <div class="col-lg-4 col-12">
+                            <div class="grid-img1">
+                                <div class="layer-1">
+                                    <div class="row bottom-text my-2 w-100">
+                                        <div class="col-8">
+                                            <h1 class="ml-2 mt-3">Pablo’s Ibiza Grid</h1>
+                                        </div>
+                                        <div class="col-4 text-lg-center text-right">
+                                            <a href="grid-design-1.html"><button class="go-to-grid-btn mt-4">Go to
+                                                    Grid</button></a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-4  mt-lg-0 mt-2 col-12">
+                            <div class="grid-img2">
+                                <div class="layer-1">
+                                    <div class="row bottom-text my-2 w-100">
+                                        <div class="col-8">
+                                            <h1 class="ml-2 mt-3">Georgia&apos;s London Grid</h1>
+                                        </div>
+                                        <div class="col-4 text-lg-center text-right">
+                                            <a href="grid-design-1.html"><button class="go-to-grid-btn mt-4">Go to
+                                                    Grid</button></a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-4  mt-2 mt-lg-0 col-12">
+                            <div class="grid-img3">
+                                <div class="layer-1">
+                                    <div class="row bottom-text my-2 w-100">
+                                        <div class="col-8">
+                                            <h1 class="ml-2 mt-3">Carla&apos;s Paris Grid</h1>
+                                        </div>
+                                        <div class="col-4 text-lg-center text-right">
+                                            <a href=""><button class="go-to-grid-btn mt-4">Go to Grid</button></a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-4  mt-2 col-12">
+                            <div class="grid-img4">
+                                <div class="layer-1">
+                                    <div class="row bottom-text my-2 w-100">
+                                        <div class="col-9">
+                                            <h1 class="ml-2 mt-3">Harry&apos;s Mykonos Grid</h1>
+                                        </div>
+                                        <div class="col-3 pl-0 text-lg-center text-right">
+                                            <a href=""><button class="go-to-grid-btn mt-4">Go to Grid</button></a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-4  mt-2 col-12">
+                            <div class="grid-img5">
+                                <div class="layer-1">
+                                    <div class="row bottom-text my-2 w-100">
+                                        <div class="col-8">
+                                            <h1 class="ml-2 mt-3">Max&apos;s Tulum Grid</h1>
+                                        </div>
+                                        <div class="col-4 text-lg-center text-right">
+                                            <a href=""><button class="go-to-grid-btn mt-4">Go to Grid</button></a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-4  mt-2 col-12">
+                            <div class="grid-img6">
+                                <div class="layer-1">
+                                    <div class="row bottom-text w-100 my-2">
+                                        <div class="col-8 ">
+                                            <h1 class="ml-2 mt-3">Johan&apos;s Tuscany Grid</h1>
+                                        </div>
+                                        <div class="col-4 pl-0 text-lg-center text-right ">
+                                            <a href=""><button class="go-to-grid-btn mt-4">Go to Grid</button></a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!--------------------end of fulfil souring request--------->
+	
+	
+	
+    <!--------------------end of fulfil souring request--------->
+    <!--  Modal -->
+    
+	   @include("stylist.postloginview.partials.footer.footerjs")
+ 
--- routes/web.php
+++ routes/web.php
@@ -25,12 +25,11 @@ use App\Http\Controllers\CreateGridController as CreateGridController;
   //Route::domain('stylist.com')->group(function () {
   Route::get('/loadgridview',[CreateGridController::class,'loadgridview']);
   Route::post('/add-grid',[CreateGridController::class,'add_grid']);
+  Route::post('/view-grid',[CreateGridController::class,'view_grid']);
   Route::post('/get-grid-data','App\Http\Controllers\CreateGridController@get_grid_data');
  // });
  //stylist Section Start
  Route::get('/', [StylistWebsite::class, 'index']);
- Route::get('/sign-up', [StylistWebsite::class, 'index']);
-
  Route::get('/stylist-registration', [StylistWebsite::class, 'stylistRegistration']);
  Route::post('/check-stylist-existance', [StylistWebsite::class, 'checkStylistExistance']);
  Route::post('/add-stylist', [StylistWebsite::class, 'addStylist']);
@@ -45,13 +44,11 @@ use App\Http\Controllers\CreateGridController as CreateGridController;
  Route::post('/stylist-fulfill-source-request-post', [Stylist::class, 'stylistFulfillSourceRequestPost']);
  Route::get('/stylist-source-request-submit', [Stylist::class, 'stylistSourceRequestSubmit']);
  Route::get('/stylist-create-source-request', [Stylist::class, 'stylistCreateSourceRequest']);
- Route::post('/get-stylist-brands', [Website::class, 'getStylistBrandList']);
+ Route::post('/get-stylist-brands', [Stylist::class, 'getStylistBrandList']);
  Route::post('/stylist-submit-request-post', [Stylist::class, 'stylistSubmitRequestPost']);
  Route::get('/stylist-submit-request-complete', [Stylist::class, 'stylistSubmitRequestComplete']);
  Route::get('/stylist-offer-received/{title}', [Stylist::class, 'stylistOfferReceived']);
- Route::post('/stylist-accept-offer', [Stylist::class, 'stylistAcceptOffer']);
- Route::get('/stylist-offer-accepted', [Stylist::class, 'stylistOfferAcceptedSuccessful']);
- Route::post('/stylist-decline-offer', [Stylist::class, 'stylistDeclineOffer']);
+
  
  
  //stylist section End
@@ -76,7 +73,7 @@ Route::get('/member-grid', [Member::class, 'memberGrid']);
 Route::get('/member-grid-details', [Member::class, 'memberGridDetails']);
 Route::get('/member-orders', [Member::class, 'memberOrders']);
 Route::get('/member-submit-request', [Member::class, 'memberSubmitRequest']);
-Route::post('/get-brands-list', [Website::class, 'getBrandList']);
+Route::post('/get-brands', [Member::class, 'getBrandList']);
 Route::post('/member-submit-request-post', [Member::class, 'memberSubmitRequestPost']);
 
 //member section End
