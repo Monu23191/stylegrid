c4e2b0afe067cf600f1856633e6641e187985b5d
monu <monu.joon@unoecom.com>
2022-11-14 14:35:49 +0530

    mm

--- routes/web.php
+++ routes/web.php
@@ -25,6 +25,7 @@ use App\Http\Controllers\CreateGridController as CreateGridController;
   //Route::domain('stylist.com')->group(function () {
   Route::get('/loadgridview',[CreateGridController::class,'loadgridview']);
   Route::post('/add-grid',[CreateGridController::class,'add_grid']);
+  Route::post('/get-grid-data','App\Http\Controllers\CreateGridController@get_grid_data');
  // });
  //stylist Section Start
  Route::get('/', [StylistWebsite::class, 'index']);
